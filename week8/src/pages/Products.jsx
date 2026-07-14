bf63525531fc917c116f3a94819f82f3dbc8aaa0
MohammedLababidi <134704955+MohammedLababidi@users.noreply.github.com>
2026-07-14 13:34:43 +0300

    Update Products.jsx

--- week8/src/pages/Products.jsx
+++ week8/src/pages/Products.jsx
@@ -1,26 +1,14 @@
 import Card from "../components/Card";
 import Button from "../components/Button";
-import { useState,useMemo, useEffect } from "react";
-import FetchData from "../customHooks/FetchData";
+import { useState, useMemo } from "react";
 
 
-function Products() {
+function Products({ products, currency }) {
   const [category, setCategory] = useState('all')
-  const [products, setProducts] = useState([]);
   const filteredProducts = useMemo(() => {
     return category === 'all' ? products : products.filter(product => product.category === category)
   }, [products, category])
 
-
-  // ! i guess you fetched all the data once? , very wrong to fetch all the data once , we dont even have a tab called (All)
-  // ! its better to fetch only the triggered data , but its ok for now , good one to use usememo for big data like this
-  useEffect(() => {
-    FetchData()
-      .then((response) => {
-        setProducts(response.data);
-      })
-      .catch((error) => console.log(error));
-  }, []);
   return (
     <>
     <div className="container">
@@ -31,7 +19,7 @@ function Products() {
       <Button className={`filter-btn rounded-5 fw-bold ${category === "women's clothing" ? 'active-2' : ''}`} onClick={() => setCategory("women's clothing")}>Women</Button>
       <Button className={`filter-btn rounded-5 fw-bold ${category === "electronics" ? 'active-2' : ''}`} onClick={() => setCategory("electronics")}>Electronics</Button>
     </div>
-    <Card products={filteredProducts} />
+    <Card products={filteredProducts} currency={currency} />
     </div>
    
     </>
